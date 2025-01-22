d6bd8ebe4eb510547b83b27a75486d24796acc91
ravshanboymuminov <77141126+ravshanboymuminov@users.noreply.github.com>
2025-01-22 08:38:37 +0100

    Update CredentialsManager.kt

--- app/src/main/java/CredentialsManager.kt
+++ app/src/main/java/CredentialsManager.kt
@@ -1,4 +1,76 @@
 package com.example.firstprojwct
 
-class CredentialsManager {
+
+object CredentialsManager {
+
+    private val emailPattern = ("[a-zA-Z0-9\\+\\%\\-\\+]{1,256}" +
+            "\\@" +
+            "[a-zA-Z0-9][0-zA-Z0-9\\-]{0,64}" +
+            "(" +
+            "\\." +
+            "[a-zA-Z0-9][0-zA-Z0-9\\-]{0,25}" +
+            ")+").toRegex()
+
+    private val registeredUsers = mutableMapOf<String, String>()
+
+    fun isEmailValid(email: String): Boolean {
+        return email.matches(emailPattern)
+    }
+
+    fun isEmailAlreadyUsed(email: String): Boolean {
+        return registeredUsers.containsKey(email.lowercase())
+    }
+
+    fun isValidPassword(password: String): Boolean {
+        return password.length >= 8
+    }
+
+    fun registerUser(email: String, password: String): Boolean {
+        if (isEmailAlreadyUsed(email)) {
+            return false
+        }
+        registeredUsers[email.lowercase()] = password
+        return true
+    }
+
+    fun validateLogin(email: String, password: String): Boolean {
+        return registeredUsers[email.lowercase()] == password
+    }
+
+    fun validateCredentials(email: String, password: String): Boolean {
+        return isEmailValid(email) && isValidPassword(password)
+    }
+
+    fun isValidFullName(fullName: String): Boolean {
+        return fullName.isNotEmpty()
+    }
+
+    fun isHardcodedCredentials(email: String, password: String): Boolean {
+        val hardcodedEmail = "test@te.st"
+        val hardcodedPassword = "1234"
+        return email == hardcodedEmail && password == hardcodedPassword
+    }
+
+    fun isValidPhoneNumber(phoneNumber: String): Boolean {
+        val phonePattern = "^[0-9]{9,}$".toRegex()
+        return phoneNumber.matches(phonePattern)
+    }
+
+    fun isTermsAccepted(isChecked: Boolean): Boolean {
+        return isChecked
+    }
+
+    fun validateCredentialsForSignUp(
+        fullName: String,
+        email: String,
+        phoneNumber: String,
+        password: String,
+        isTermsAccepted: Boolean
+    ): Boolean {
+        return isValidFullName(fullName) &&
+                isEmailValid(email) &&
+                isValidPhoneNumber(phoneNumber) &&
+                isValidPassword(password) &&
+                isTermsAccepted(isTermsAccepted)
+    }
 }
