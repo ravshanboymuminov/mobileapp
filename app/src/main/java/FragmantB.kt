a6ce82a0413d5c38d861cac0977b0e2eb3e0b723
ravshanboymuminov <77141126+ravshanboymuminov@users.noreply.github.com>
2025-01-22 06:11:23 +0100

    update

---
+++ app/src/main/java/FragmantB.kt
@@ -0,0 +1,5 @@
+
+package com.example.firstprojwct
+import androidx.fragment.app.Fragment
+
+class FragmentB : Fragment(R.layout.fragment_b)
---
+++ app/src/main/java/FragmentA.kt
@@ -0,0 +1,7 @@
+package com.example.firstprojwct
+
+
+
+import androidx.fragment.app.Fragment
+
+class FragmentA : Fragment(R.layout.fragment_a)
