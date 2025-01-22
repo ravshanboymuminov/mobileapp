2c95ae21f80961ce109251cc39cbebdf3f0be677
ravshanboymuminov <77141126+ravshanboymuminov@users.noreply.github.com>
2025-01-22 08:39:20 +0100

    Update SecondActivity.kt

--- app/src/main/java/SecondActivity.kt
+++ app/src/main/java/SecondActivity.kt
@@ -1,4 +1,24 @@
 package com.example.firstprojwct
 
-class SecondActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_create_account)
+
+        val myTextView2: TextView = findViewById(R.id.tvRegisterNoww)
+        myTextView2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+    }
 }
